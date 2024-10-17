f879ba347c0c3d9b3478ea5667b11dbb5e035b25
Sameer ansari <ansarit209@gmail.com>
2024-10-17 23:38:03 +0530

    hey CodeBase Please add 11 project

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,17 @@
-const getTheTitles = function() {
-
+const books = [
+    {
+      title: 'Book',
+      author: 'Name'
+    },
+    {
+      title: 'Book2',
+      author: 'Name2'
+    }
+  ]
+const getTheTitles = function(books) {
+    let titles = books.map((Book)=> Book.title)
+    return titles
 };
-
+console.log(getTheTitles(books))
 // Do not edit below this line
 module.exports = getTheTitles;
